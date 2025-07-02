90b361d7e84a2d01db157ffa543879f07711a0b4
elijah <elijah.b.crain@gmail.com>
2025-07-02 12:43:09 -0400

    changes for cfrapp

--- MyWorker.hpp
+++ MyWorker.hpp
@@ -22,7 +22,6 @@ class MyWorker : public Worker
 
   void doWork(uint32_t epochs, uint32_t iterations)
   {
-    minimizer.printPreflopStats();
     using enum Worker::State;
     if (state == PAUSED)
       // treat as resume
